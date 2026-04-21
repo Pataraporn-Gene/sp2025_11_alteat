54a65fa95d2f8eb4f0bf299d4f2054ad0ff2d9b7
unknown <pataraporn.penp@gmail.com>
2026-04-21 13:56:45 +0700

    Updata chatbot page

--- AltEat/src/pages/ChatbotPage.tsx
+++ AltEat/src/pages/ChatbotPage.tsx
@@ -34,8 +34,8 @@ interface ChatSession {
   created_at: string;
 }
 
-// const N8N_WEBHOOK_URL = "https://primary-production-6fdb0.up.railway.app/webhook/f1725c42-d6af-4b84-adba-86fb367f9f3b/chat";
-const N8N_WEBHOOK_URL = "http://localhost:5678/webhook/f1725c42-d6af-4b84-adba-86fb367f9f3b/chat";
+const N8N_WEBHOOK_URL = "https://primary-production-6fdb0.up.railway.app/webhook/f1725c42-d6af-4b84-adba-86fb367f9f3b/chat";
+// const N8N_WEBHOOK_URL = "http://localhost:5678/webhook/f1725c42-d6af-4b84-adba-86fb367f9f3b/chat";
 
 function ChatbotPage() {
   const { t } = useTranslation("chatbot");
